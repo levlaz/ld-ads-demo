a3ee76740d95037ef62e180da632086156803301
Lev Lazinskiy <lev@launchdarkly.com>
2018-08-09 09:07:59 -0700

    Add CLI

--- src/main/java/org/levlaz/adsdemo/App.java
+++ src/main/java/org/levlaz/adsdemo/App.java
@@ -7,24 +7,30 @@ import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 
 import org.flywaydb.core.Flyway;
+import org.levlaz.adsdemo.connectors.Console;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import okhttp3.Headers;
-import org.levlaz.adsdemo.connectors.Console;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 /**
  * LaunchDarkly ADS Consumer Reference Implementation
  *
  */
-public class App 
-{
+@Command(name = "ld-ads-demo", mixinStandardHelpOptions = true)
+public class App implements Runnable {
+    @Option(names = {"-c" , "--connector"}, description = "Connector to Use")
+    String connector = "default";
+
     private static final Logger logger =
         LoggerFactory.getLogger(App.class.getName());
 
-    public static void main( String[] args ) throws InterruptedException
-    {
+    public void run() {
+        EventHandler eventHandler = null;
         Dotenv dotenv = Dotenv.configure()
             .ignoreIfMissing()
             .load();
@@ -41,8 +47,21 @@ public class App
                 logger.error("Unable to run Migration: " + e);
             }
         }
-        
-        EventHandler eventHandler = new Console();
+
+        switch (connector) {
+            case "console":
+                logger.info("Starting ADS consumer with console connector.");
+                eventHandler = new Console();
+                break;
+            case "default":
+                logger.info("Starting ADS consumer with default connector.");
+                eventHandler = new SimpleEventHandler();
+                break;
+            default: 
+                logger.error("Invalid Connector, falling back to default.");
+                break;
+        }
+
         String url = String.format("https://firehose.launchdarkly.com");
         Headers headers = new Headers.Builder()
             .add("Authorization", dotenv.get("LD_SDK_KEY"))
@@ -57,6 +76,13 @@ public class App
             eventSource.start();
 
             TimeUnit.MINUTES.sleep(10);
+        } catch (Exception e) {
+            logger.error("Error: " + e);
         }
+    }
+
+    public static void main( String[] args )
+    {
+        CommandLine.run(new App(), System.out, args);
     }
 }
